af1d22b62de9b785145b0792ded06050d0c113a2
kazu0112 <kazuma0112@icloud.com>
2026-07-27 14:33:32 +0900

    UserCreateAction.java作成

--- src/main/java/com/diworksdev/ecsite/action/LoginAction.java
+++ src/main/java/com/diworksdev/ecsite/action/LoginAction.java
@@ -26,6 +26,11 @@ public class LoginAction extends ActionSupport implements SessionAware{
 		if(((LoginDTO) session.get("loginUser")).getLoginFlg()) {
 			result = SUCCESS;
 			BuyItemDTO buyItemDTO = buyItemDAO.getBuyItemInfo();
+
+			System.out.println("id=" + buyItemDTO.getId());
+			System.out.println("name=" + buyItemDTO.getItemName());
+			System.out.println("price=" + buyItemDTO.getItemPrice());
+			
 			session.put("login_user_id", loginDTO.getLoginId());
 			session.put("id", buyItemDTO.getId());
 			session.put("buyItem_name", buyItemDTO.getItemName());
